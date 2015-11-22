ff89c42d5b675509204fcf5c3e77a74a788c0cd8
Ryan Lombardo <ryan.lombardo@buzzfeed.com>
2015-11-22 03:16:35 -0500

    Added image viewer for shipping record

---
+++ frickfeed/static/js/image_viewer.js
@@ -0,0 +1,16 @@
+function init() {
+  var map = L.map('shippingRecord', {
+    maxZoom: 24,
+    minZoom: 1,
+    crs: L.CRS.Simple
+  }).setView([0, 0], 1);
+
+  map.setMaxBounds(new L.LatLngBounds([0,500], [500,0]));
+
+  var imageUrl = 'static/img/shipping_records/frick_1.png';
+  var imageBounds = [[250,0], [0,250]];
+
+  L.imageOverlay(imageUrl, imageBounds).addTo(map);
+}
+
+init();
