60e3a9d6531b49297cffdd95894699a85004b09d
Greg <greg.thompson@buzzfeed.com>
2015-11-21 23:11:49 -0500

    Adding base templates and related url.

--- frickfeed/frickfeed/urls.py
+++ frickfeed/frickfeed/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from apps.records.views import IndexView
+from django.views.generic import TemplateView
 import routers.v1 as v1router
 
 
 urlpatterns = [
-    url(r'^$', IndexView),
+    # Base Application
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^v1/', include(v1router.router.urls))
 ]
