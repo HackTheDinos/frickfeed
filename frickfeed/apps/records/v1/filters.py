4cb7a69d3c15cf45d2d3ab86e0d1baea76e2dafe
benjwg <benj.wg@gmail.com>
2015-11-22 03:20:16 -0500

    fix filters

--- frickfeed/apps/records/v1/filters.py
+++ frickfeed/apps/records/v1/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db import models
 from apps.records.models import Record, Specimen
 
 class RecordFilter(django_filters.FilterSet):
@@ -7,13 +8,22 @@ class RecordFilter(django_filters.FilterSet):
     min_date_recorded = django_filters.DateTimeFilter(name='date_recorded',lookup_type='gte')
     max_date_recorded = django_filters.DateTimeFilter(name='date_recorded',lookup_type='lte')
 
+    filter_overrides = {
+        models.CharField: {
+            'filter_class': django_filters.CharFilter,
+            'extra': lambda f: {
+                'lookup_type': 'icontains',
+            }
+        }
+    }
+
     class Meta:
         model = Record
         fields = {
-            'shipping_point': ['icontains'],
-            'collector_name_first': ['icontains'],
-            'collector_name_last': ['icontains'],
-            'season_at': ['icontains'],
+            'shipping_point': ['exact'],
+            'collector_name_first': ['exact'],
+            'collector_name_last': ['exact'],
+            'season_at': ['exact'],
             'date_recorded': ['exact'],
             'date': ['exact'],
         }
