be9f04aa38f24975f4fc19576c60d0cc353cc682
benjwg <benj.wg@gmail.com>
2015-11-21 23:14:00 -0500

    add stuff

--- frickfeed/apps/records/v1/viewsets.py
+++ frickfeed/apps/records/v1/viewsets.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from apps.records.v1.serializers import RecordSerializer, SpecimenSerializer
+from apps.records.v1.serializers import RecordSerializer, RecordListSerializer, SpecimenSerializer
 from apps.records.models import Record, Specimen
 
 
@@ -7,6 +7,9 @@ class RecordViewSet(ModelViewSet):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
 
+    def list(self, request):
+        self.serializer_class = RecordListSerializer
+        return super(RecordViewSet, self).list(request)
 
 class SpecimenViewSet(ModelViewSet):
     queryset = Specimen.objects.all()
