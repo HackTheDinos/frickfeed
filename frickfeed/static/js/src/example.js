5e9a32f25e397c9ec2784d5718c2b712cd3bf63d
stephtzhang <stephtzhang@gmail.com>
2015-11-22 14:09:30 -0500

    Handle specimen updates in parent module

--- frickfeed/static/js/src/example.js
+++ frickfeed/static/js/src/example.js
@@ -1,5 +1,6 @@
-// TODO: post to specimens endpoint
-///////// Record
+// TODO: separate modules into different file
+
+///////// Record /////////
 var RecordForm = React.createClass({
   getInitialState: function() {
     // TODO: make resetState a constant
@@ -11,7 +12,8 @@ var RecordForm = React.createClass({
         date_recorded: '',
         date: '',
         season_at: '',
-        shipping_point: ''
+        shipping_point: '',
+        specimens: []
     }
     return resetState;
   },
@@ -39,6 +41,16 @@ var RecordForm = React.createClass({
   handleSeasonAtChange: function(e) {
     this.setState({season_at: e.target.value});
   },
+  handleSpecimenChange: function(specimenState) {
+    // specimens are uniquely identified by client_identifier
+    for(var i=0; i < this.state.specimens.length; i++) {
+        if ( this.state.specimens[i].client_identifier === specimenState.client_identifier ) {
+            this.state.specimens[i] = specimenState;
+            return;
+        }
+    }
+    this.state.specimens.push(specimenState);
+  },
   handleSubmit: function(e) {
     e.preventDefault();
     // validate fields are filled
@@ -53,10 +65,11 @@ var RecordForm = React.createClass({
 
     // TODO: validate by iterating through these and calling a validate function
     if (!collector_name_first || !collector_name_last || !location || !box || !date_recorded || !date || !season_at || !shipping_point ) {
-      debugger;
       // TODO: handle validation appropriately
       return;
     }
+
+    // TODO: fire dispatcher event to delete specimens
 
     // reset input fields on submit
     var resetState = {
@@ -67,14 +80,19 @@ var RecordForm = React.createClass({
         date_recorded: '',
         date: '',
         season_at: '',
-        shipping_point: ''
+        shipping_point: '',
+        specimens: []
     }
     this.setState(resetState);
     this.props.onRecordSubmit(this.state);
   },
   render: function() {
+    var specimenProps = {
+        onChange: this.handleSpecimenChange
+    }
     return (
       <form className="recordForm" onSubmit={this.handleSubmit}>
+        <h2>Record Details</h2>
         <input
           type="text"
           placeholder="First name"
@@ -123,17 +141,90 @@ var RecordForm = React.createClass({
           value={this.state.season_at}
           onChange={this.handleSeasonAtChange}
         />
-        <input type="submit" value="Post" />
+        <SpecimenInputFields {...specimenProps} />
+        <input type="submit" value="Save" />
       </form>
     );
   }
 });
 
-///////// RecordBox
+// TODO: add ability to append multiple specimen input fields
+var SpecimenInputFields = React.createClass({
+  getInitialState: function() {
+    var resetState = {
+        client_identifier: (Math.random() * 1000000).toString(),
+        amnh_catalog_a: '',
+        amnh_catalog_b: '',
+        field_no: '',
+        description: '',
+        location: '',
+    };
+    return resetState;
+  },
+  handleAmnhCatalogAChange: function(e) {
+    debugger;
+    this.setState({amnh_catalog_a: e.target.value});
+    this.props.onChange(this.state);
+  },
+  handleAmnhCatalogBChange: function(e) {
+    this.setState({amnh_catalog_b: e.target.value});
+    this.props.onChange(this.state);
+  },
+  handleFieldNoChange: function(e) {
+    this.setState({field_no: e.target.value});
+    this.props.onChange(this.state);
+  },
+  handleDescriptionChange: function(e) {
+    this.setState({description: e.target.value});
+    this.props.onChange(this.state);
+  },
+  handleLocationChange: function(e) {
+    this.setState({location: e.target.value});
+    this.props.onChange(this.state);
+  },
+  render: function() {
+    return (
+    <section class="kitten" id="kitten">
+        <h3>Specimen Details</h3>
+        <input
+          type="text"
+          placeholder="AMNH Catalog A"
+          value={this.state.amnh_catalog_a}
+          onChange={this.handleAmnhCatalogAChange}
+        />
+        <input
+          type="text"
+          placeholder="AMNH Catalog B"
+          value={this.state.amnh_catalog_b}
+          onChange={this.handleAmnhCatalogBChange}
+        />
+        <input
+          type="number"
+          placeholder="Field No."
+          value={this.state.field_no}
+          onChange={this.handleFieldNoChange}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={this.state.description}
+          onChange={this.handleDescriptionChange}
+        />
+        <input
+          type="text"
+          placeholder="Location"
+          value={this.state.location}
+          onChange={this.handleLocationChange}
+        />
+    </section>
+    );
+  }
+});
+
+///////// RecordBox /////////
 var RecordBox = React.createClass({
   handleRecordSubmit: function(record) {
     // TODO: optimistically append record here
-
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -141,7 +232,6 @@ var RecordBox = React.createClass({
       data: record,
       success: function(data) {
         // TODO: handle successful record post req appropriately
-        debugger;
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
@@ -157,7 +247,7 @@ var RecordBox = React.createClass({
   }
 });
 
-///////// render RecordBox
+///////// render RecordBox /////////
 ReactDOM.render(
   <RecordBox url="/v1/records/" />,
   document.getElementById('content')
